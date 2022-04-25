ed3cfdb1d1907a49f4c339dcd5f0e0e6e3652835
Hackinator-2003 <71487623+Hackinator-2003@users.noreply.github.com>
2022-04-25 02:03:47 +0200

    Slider ready

---
+++ components/Bar.vue
@@ -0,0 +1,60 @@
+<template>
+    <div id="header">
+        <a href="."><img src="../assets/Crop_mikuni_logo.svg" class="logo"></a>
+        <ul>
+            <li><a href=".">Accueil</a></li>
+            <li><a href="#">Notre Projet</a></li>
+            <li><a href="#">Nos Sites</a></li>
+            <li><a href="#">Le Jeu</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Bar"
+}
+</script>
+
+<style lang="scss">
+#header {
+    width:100%;
+    flex: 1 1 10%;
+    display: flex;
+    padding-bottom: 2vh;
+    margin: 0;
+    background-color: #fc929e;
+    position: fixed;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction:column;
+    z-index: 10;
+
+    .logo {
+        flex-basis:20vw;
+        padding: 2vh;
+        max-width: 20vw;
+        min-width: 8vw;
+        flex:1 1 auto;
+    }
+
+    ul {
+        flex: 1 1 80%;
+        position: relative;
+        justify-content: center;
+        list-style: none;
+
+        li {
+            float: left;
+
+            a {
+                margin: 0 1.3vw;
+                padding-bottom: 10%;
+                text-decoration: none;
+                font-size: 1.5vw;
+            }
+        }
+    }
+}
+
+</style>
---
+++ components/DownBut.vue
@@ -0,0 +1,28 @@
+<template>
+    <a href="#downloadbut"><button class="button">Télécharger</button></a>
+</template>
+
+<script>
+export default {
+    name: "DownBut"
+}
+</script>
+
+<style scoped>
+.button {
+    padding: 1.3vh 5vw;
+    margin: 2vh 10vw;
+    color: #10324d;
+    background-color: #ffdfe0;
+    font-family: sans-serif;
+    text-align: center;
+    position: relative;
+    text-decoration: none;
+    display: inline-block;
+    border: 1px solid;
+}
+.button:hover {
+    color: #ffdfe0;
+    background-color: #10324d;
+}
+</style>
