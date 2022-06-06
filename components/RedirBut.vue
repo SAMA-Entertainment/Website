80d011f887cda3dd8979608fdd2026f39cebc00c
Hackinator-2003 <71487623+Hackinator-2003@users.noreply.github.com>
2022-06-06 15:14:55 +0200

    Fix pb WS

---
+++ components/RedirBut.vue
@@ -0,0 +1,35 @@
+<template>
+    <a :href="require(link)">
+        <button class="button"><h2>{{text}}</h2></button></a>
+</template>
+
+<script>
+export default {
+    name: "DownBut",
+    props:{
+        text:String,
+        link:String,}
+}
+</script>
+
+<style scoped>
+.button {
+    padding: 1.3vh 5vw;
+    margin: 2vh 10vw;
+    color: #10324d;
+    font-weight: bold;
+    background-color: #ffdfe0;
+    font-family: sans-serif;
+    text-align: center;
+    position: relative;
+    text-decoration: none;
+    display: inline-block;
+    border: 1vh solid #10324d;
+    border-radius: 10px;
+}
+.button:hover {
+    color: #ffdfe0;
+    background-color: #10324d;
+    border: 1vh solid #ffdfe0;
+}
+</style>
